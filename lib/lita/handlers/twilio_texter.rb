a8710d0b731d23c1d73c5bdf062f689ebcbd5837
Daniel J. Pritchett <dpritchett@gmail.com>
2017-11-21 08:56:48 -0600

    Twilio testing done

--- lib/lita/handlers/twilio_texter.rb
+++ lib/lita/handlers/twilio_texter.rb
@@ -4,8 +4,6 @@ require 'pry'
 module Lita
   module Handlers
     class TwilioTexter < Handler
-      # insert handler code here
-
       Lita.register_handler(self)
 
       route /^text\s+(\d+)\s+(.+)$/i,
@@ -26,12 +24,12 @@ module Lita
 
       def send_text(message)
         _, to, body = message.match_data.to_a
-        results = twilio_sms(to: to, body: body)
+        results = send_twilio_sms(to: to, body: body)
 
         message.reply "Sent message to #{to}"
       end
 
-      def twilio_sms(to:, body:)
+      def send_twilio_sms(to:, body:)
         response = client.api.account.messages.create(
           from: send_from_number,
           to: to,
--- spec/lita/handlers/twilio_texter_spec.rb
+++ spec/lita/handlers/twilio_texter_spec.rb
@@ -14,6 +14,22 @@ describe Lita::Handlers::TwilioTexter, lita_handler: true do
       expect(replies.last).to eq('Sent message to 19016052970')
     end
   end
+
+  describe ':client' do
+    it 'should return a logged-in Twilio client' do
+      clt = subject.client
+      expect(clt.account_sid.empty?).to be_falsey
+      expect(clt.auth_token.empty?).to be_falsey
+    end
+  end
+
+  # dependent on my area code, naturally
+  describe ':send_from_number' do
+    it 'should return a Memphis-area phone number' do
+      number = subject.send_from_number
+      expect(number.start_with?('+1901')).to be_truthy
+    end
+  end
 end
 
 
